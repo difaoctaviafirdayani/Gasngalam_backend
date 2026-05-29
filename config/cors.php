9b83f0a6e6fc8cb02c7057bc3554fb24cdc28a73
difaoctaviafirdayani <difaoctaviafirdayani@exampel.com>
2026-05-29 14:04:49 +0700

    update

--- config/cors.php
+++ config/cors.php
@@ -3,15 +3,10 @@
 return [
     'paths'                    => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods'          => ['*'],
-    'allowed_origins'          => [
-        'http://localhost:5173',
-        'http://localhost:5174',
-        'http://127.0.0.1:5173',
-        'http://127.0.0.1:8000',
-    ],
+    'allowed_origins'          => ['*'],
     'allowed_origins_patterns' => [],
     'allowed_headers'          => ['*'],
     'exposed_headers'          => [],
     'max_age'                  => 0,
-    'supports_credentials'     => true,
+    'supports_credentials'     => false,
 ];
